815a707f02a0b11db5584c39d3013369dfb69b1d
mMoukhtar <mohamed.moukhtar@outlook.com>
2020-05-14 23:30:04 +0200

    Adding Lists

---
+++ src/Collections/CsvReader.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace Collections
+{
+    class CsvReader
+    {
+        private string filePath;
+
+        public CsvReader(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public List<Country> ReadAllCountries(){
+            using (var reader = new StreamReader(filePath)){
+                reader.ReadLine();
+                var list = new List<Country>();
+                while (reader.Peek() > 0) 
+                {
+                    var stringLine = reader.ReadLine();
+                    list.Add(ReadCountryFromCsvLine(stringLine));
+                }
+                return list;
+            }
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+
+            using (var reader = new StreamReader(filePath))
+            {
+                reader.ReadLine();
+                for (var i = 0 ; i < countries.Length ; i++)
+                {
+                    var stringLine = reader.ReadLine();
+                    countries[i] = ReadCountryFromCsvLine(stringLine);
+                }
+            }
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            var parts = csvLine.Split(",");
+            var name=string.Empty;
+            var code=string.Empty;
+            var region=string.Empty;
+            var popText=string.Empty;
+            var population = 0;
+            switch(parts.Length)
+            {
+                case 4:
+                {
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+                }
+                case 5:
+                {
+                    name = parts[0] + parts[1];
+                    name = name.Replace("\"",null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                }
+                default:
+                {
+                    throw new InvalidDataException($"Invalid Country Data {csvLine}");
+                }
+            }
+            int.TryParse(popText,out population);
+            return new Country(name,code,region,population);
+        }
+    }
+}
--- src/Collections/Program.cs
+++ src/Collections/Program.cs
@@ -6,7 +6,18 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var filePath = @"c:\Users\Moham\CSharp\Collections1\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            var countries = reader.ReadAllCountries();
+
+            var lilliput = new Country("Liliput","LIL","Somewhere",2000000);
+            var lilliputIndex = countries.FindIndex(x=>x.Population < 2000000);
+            countries.Insert(lilliputIndex,lilliput);
+
+            foreach (var Country in countries){
+                Console.WriteLine($"{Country.Population}: {Country.Name}");
+            }
         }
     }
 }
